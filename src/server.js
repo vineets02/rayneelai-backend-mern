96f75b6a754985b436f48479a8bc79e788174fda
gms-vineet <vineet.shrivastava@getmysolutions.in>
2026-03-24 15:48:37 +0530

    Initial backend-mern commit

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const app = require('./app');
 const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0';
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+  app.listen(PORT, HOST, () => {
+    console.log(`Server running on ${HOST}:${PORT}`);
   });
 }).catch(err => {
   console.error('Failed to connect to database. Server not started.', err);
